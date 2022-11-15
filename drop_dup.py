20a50af807227aa9c6b26ad94a48fce2971854a5
eight <eight@eightnoiMac.local>
2022-11-15 16:50:19 +0900

    [fix]:

--- drop_dup.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("test.csv")
-df = df.drop_duplicates()
-df["nearest_station"] = df["nearest_station"].map(lambda x: x.split(" ")[0])
-df.to_csv("wine_tabelog.csv", index=False)
